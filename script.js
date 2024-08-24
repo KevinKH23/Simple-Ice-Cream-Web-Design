d922879ce72b0c89a1d612ed80193b8140c29e6f
Kevin <kevinkurniawan265@gmail.com>
2024-08-24 17:18:49 +0700

    add flavors section

--- script.js
+++ script.js
@@ -15,6 +15,7 @@ document.addEventListener("click", function (e) {
   }
 });
 
+// Header ke atas atau bawah ketika layar di scroll
 let lastScrollPosition = 0;
 
 document.addEventListener("scroll", function () {
@@ -31,3 +32,72 @@ document.addEventListener("scroll", function () {
 
   lastScrollPosition = currentScrollPosition;
 });
+
+// Image flavors bergeser ketika arrow diklik
+document.addEventListener("DOMContentLoaded", function () {
+  const images = [
+    { src: "img/flavor1.png", name: "Vanilla" },
+    { src: "img/flavor2.png", name: "Chocolate" },
+    { src: "img/flavor3.png", name: "Strawberry" },
+    { src: "img/flavor4.png", name: "Chocolate Caramel" },
+    { src: "img/flavor5.png", name: "Tiramisu" },
+    { src: "img/flavor6.png", name: "Chocolate Chip" },
+    { src: "img/flavor7.png", name: "Pistacchio" },
+  ];
+
+  let currentIndex = 0;
+
+  const imgElement = document.getElementById("flavors-img");
+  const imgNameElement = document.getElementById("flavors-img-name");
+  const prevArrow = document.getElementById("prev-arrow");
+  const nextArrow = document.getElementById("next-arrow");
+
+  function updateImage() {
+    imgElement.src = images[currentIndex].src;
+    imgNameElement.textContent = images[currentIndex].name;
+  }
+
+  prevArrow.addEventListener("click", function () {
+    currentIndex = (currentIndex - 1 + images.length) % images.length;
+    updateImage();
+  });
+
+  nextArrow.addEventListener("click", function () {
+    currentIndex = (currentIndex + 1) % images.length;
+    updateImage();
+  });
+
+  // Initial load
+  updateImage();
+});
+
+// document.addEventListener("DOMContentLoaded", function () {
+//   const images = [
+//     "img/about-img.png", // Ganti dengan path gambar Anda
+//     "img/juiceimage.png",
+//     "img/smoothiesimage.png",
+//   ];
+
+//   let currentIndex = 0;
+
+//   const imgElement = document.getElementById("flavors-img");
+//   const prevArrow = document.getElementById("prev-arrow");
+//   const nextArrow = document.getElementById("next-arrow");
+
+//   // Function to update the image source
+//   function updateImage(index) {
+//     imgElement.src = images[index];
+//   }
+
+//   // Event listener for Previous arrow
+//   prevArrow.addEventListener("click", function () {
+//     currentIndex = currentIndex === 0 ? images.length - 1 : currentIndex - 1;
+//     updateImage(currentIndex);
+//   });
+
+//   // Event listener for Next arrow
+//   nextArrow.addEventListener("click", function () {
+//     currentIndex = currentIndex === images.length - 1 ? 0 : currentIndex + 1;
+//     updateImage(currentIndex);
+//   });
+// });
